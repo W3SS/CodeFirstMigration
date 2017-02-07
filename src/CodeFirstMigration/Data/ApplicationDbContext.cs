28e08ec10b888556a9a852afdd23126bf7ef3f1e
Bryan Wood <whenidev@gmail.com>
2017-02-06 16:08:07 -0800

    default project structure

---
+++ src/CodeFirstMigration/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstMigration.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/CodeFirstMigration/Data/SampleData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace CodeFirstMigration.Data
+{
+    public class SampleData
+    {
+        public async static Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetService<ApplicationDbContext>();
+            await db.Database.EnsureCreatedAsync();
+
+            // Add Sample Data Insertions
+        }
+    }
+}
